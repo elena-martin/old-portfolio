b8f74f91b1bb09e86bcd65d018193fa0082207fc
elena-martin <elenamartindesign@gmail.com>
2022-08-05 14:43:04 -0700

    Added Preloader

--- gallery-script.js
+++ gallery-script.js
@@ -107,4 +107,246 @@ function datavisSlides(){
         datavis_slides.item(1).setAttribute("style", "display: none");
         datavis_slides.item(2).setAttribute("style", "display: block");
     };
+}
+/*----- CREATE IMG LIGHTBOX -----*/
+const boxContent = document.createElement("div");
+boxContent.id = "boxContent";
+boxContent.setAttribute("onclick","lightboxClose();")
+
+const boxImg = document.createElement("img");
+boxImg.id = "boxImg";
+
+const imgContent = document.createElement("div");
+imgContent.id = "imgContent";
+
+const imgTitle = document.createElement("h2");
+imgTitle.id = "imgTitle";
+
+
+const imgDesc = document.createElement("p");
+imgDesc.id = "imgDesc";
+
+const closeLightbox = document.createElement("button");
+closeLightbox.id = "close-lightbox";
+closeLightbox.textContent="×"
+closeLightbox.setAttribute("onclick","lightboxClose();")
+
+/*----- DETERMINE WhICH SLIDESHOW & SLIDES TO MOVE -----*/
+var currentSlide = 1;
+let currentSlideshow = gfxSlideshow;
+
+let slides = currentSlideshow.children;
+
+
+function load(){
+    const lightbox = document.createElement("div");
+    lightbox.id = "lightbox";
+    lightbox.style = "display: none";
+
+    document.body.insertBefore(lightbox, nav);
+
+    lightbox.appendChild(closeLightbox);
+    lightbox.appendChild(boxContent);
+    boxContent.appendChild(boxImg);
+    lightbox.appendChild(imgContent);
+    
+    imgContent.appendChild(imgTitle);
+    imgContent.appendChild(imgDesc);
+
+    gfxSlides();
+    gitSlides();
+    datavisSlides();
 };
+
+function getFigure(e){ 
+    source = e.target.src
+    title = e.target.alt
+    let descP = e.target.nextElementSibling
+    desc = descP.textContent
+    relsource = source.replace("file:///D:/Coding/GitHub%20Repositories/portfolio/additional-projects/", "./");
+    boxImg.setAttribute("src", relsource);
+    imgTitle.textContent = title;
+    imgDesc.textContent = desc;
+}
+
+function hoverImg() {
+    const hoverOL = document.createElement("div");
+    hoverOL.classList.add("hoverOL");
+    hoverOL.width = "100%";
+    hoverOL.height = "100%";
+    hoverOL.backgroundColor = "#365673";
+    list.forEach(function(){
+        gallergyFigs.appendChild(hoverOL);
+    })
+
+};
+
+
+/*----------SLIDE COUNTER----------*/
+function prevSlide(e){
+    let classes = e.target.classList
+    if (classes.contains('gfx') == true){
+        currentSlideshow = gfxSlideshow;
+        console.log(currentSlideshow.id);
+        slideBack3();
+        gfxSlides();
+    };
+
+    if (classes.contains('git') == true){
+        currentSlideshow = gitSlideshow;
+        console.log(currentSlideshow.id);
+        slideBack2();
+        gitSlides();
+    };
+
+    if (classes.contains('data-vis') == true){
+        currentSlideshow = datavisSlideshow;
+        console.log(currentSlideshow.id);
+        slideBack3();
+        datavisSlides();
+    };
+
+    
+};
+
+function nextSlide(e){
+    let classes = e.target.classList
+    if (classes.contains('gfx') == true){
+        currentSlideshow = gfxSlideshow;
+        console.log(currentSlideshow.id);
+        slideNext3();
+        gfxSlides();
+    };
+
+    if (classes.contains('git') == true){
+        currentSlideshow = gitSlideshow;
+        console.log(currentSlideshow.id);
+        slideNext2();
+        gitSlides();
+    };
+
+    if (classes.contains('data-vis') == true){
+        currentSlideshow = datavisSlideshow;
+        console.log(currentSlideshow.id);
+        slideNext3();
+        datavisSlides();
+    };
+
+    
+};
+
+/*----------SLIDE CHANGER----------*/
+
+function slideBack3(){
+    if (currentSlide == 1){
+        currentSlide = 3;
+        console.log(currentSlide);
+    } else{
+        currentSlide -= 1;
+        console.log(currentSlide)
+    }
+}
+
+function slideBack2(){
+    if (currentSlide == 1 || currentSlide == 3 ){
+        currentSlide = 2;
+        console.log(currentSlide);
+    }
+    else{
+        currentSlide -= 1;
+        console.log(currentSlide)
+    }
+}
+
+function slideNext3(){
+    if (currentSlide == 3){
+        currentSlide = 1;
+        console.log(currentSlide);
+    } else{
+        currentSlide += 1;
+        console.log(currentSlide);
+    }
+}
+
+function slideNext2(){
+    if (currentSlide == 2 || currentSlide == 3 ){
+        currentSlide = 1;
+        console.log(currentSlide);
+    } 
+    else{
+        currentSlide += 1;
+        console.log(currentSlide);
+    }
+}
+
+/*---------- OPEN DATA VIS PDFs----------*/
+
+function openPDF1(){
+    window.open('./data-vis/PersonaliPhoneStats.pdf');
+}
+
+function openPDF2(){
+    window.open('./data-vis/TheFallOfKingMario.pdf');
+}
+
+function openPDF3(){
+    window.open('./data-vis/Rev77Infographic.pdf');
+}
+
+
+/*----------LIGHTBOX FUNCTIONS----------*/
+function lightboxOpen(){
+    lightbox.style = "display: block";
+    boxImg.setAttribute("target", "_blank");
+    disableScroll();
+}
+
+function lightboxClose(){
+    lightbox.style = "display: none";
+    boxImg.setAttribute("target", "_blank");
+    enableScroll();
+}
+
+/*----------DISABLE SCROLL IN LIGHTBOX----------*/
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+}
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+}));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+//Disable
+function disableScroll() {
+    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+    window.addEventList
+};
+
+//Enable
+function enableScroll() {
+    window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+    window.removeEventListener('touchmove', preventDefault, wheelOpt);
+    window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
--- script.js
+++ script.js
@@ -1,5 +1,49 @@
-function loadContent(){
-    console.log(window.innerWidth + "x" + window.innerHeight);
-    url = document.URL;
-    console.log(url);
+/*--- PRELOADER ---*/
+let loader = ""
+
+let thisURL=""
+let baseURL = "";
+
+let docURL = document.URL;
+let relativeURL = "";
+
+
+function init(){
+    let content = document.body;
+    console.log("loaded");
+
+    loader = document.createElement('iframe');
+    loader.id="preloader";
+
+    console.log(loader);
+    thisURL = loader.src.toString();
+    baseURL = thisURL.replace("/preloader.html", "");
+    relativeURL = docURL.replace(baseURL, "")
+
+    if (relativeURL.includes("/additional") || relativeURL.includes("/commercial")){
+        loader.src="../preloader.html";
+        content.prepend(loader);
+        console.log("tried 1st url")
+    } else {
+        loader.src="./preloader.html";
+        content.prepend(loader);
+        console.log("tried 2nd url")
+    }
+
+
+    setTimeout(() => {
+        loader.style.opacity = "0"
+    }, "1000");
+    setTimeout(() => {
+        loader.style.display = "none"
+    }, "2000");
+
+    
+};
+
+const path = require('path');
+let url = path1 = path.relative("portfolio/", document)
+
+function getLocation(){
+    console.log(window.location.pathname);
 };
